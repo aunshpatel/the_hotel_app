35dc84fe081a4152c18d08611b119717eccab56a
aunshpatel <patelaunsh28@gmail.com>
2024-09-16 22:15:23 -0400

    Create login screen ui

---
+++ lib/login_screen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:the_hotel_app/widgets/consts.dart';
+import 'package:the_hotel_app/widgets/rounded_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String email = '', pwd = '';
+  String loginEmailID = '', loginPassword = '';
+  bool showSpinner = false;
+  bool _passwordVisible = false;
+  bool _isRememberMe = false;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        drawerEnableOpenDragGesture: true,
+        appBar: AppBar(
+          actions: <Widget>[
+
+          ],
+          centerTitle: true,
+          title: const Wrap(
+            children: [
+              Text('Login ', style:TextStyle(color: kLightTitleColor),),
+              Text('Screen', style:TextStyle(color: kDarkTitleColor),),
+            ],
+          ),
+          backgroundColor: kBackgroundColor,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(24, 40, 24, 0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(
+                height: 15.0,
+              ),
+              Flexible(
+                child: HeroLogo(height:250,image:'images/the-hotel-app-high-resolution-logo.jpeg', tag:'photo'),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                onChanged:(value){
+                  email = emailController.text;
+                },
+                decoration: textInputDecoration('Enter your email',),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              TextField(
+                controller: passwordController,
+                obscureText: _passwordVisible == false ? true : false,
+                onChanged:(value){
+                  pwd = passwordController.text;
+                },
+                decoration: passwordInputDecoration(
+                    'Enter your password',
+                    _passwordVisible,
+                        (){
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    }
+                ),
+              ),
+              const SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                colour:kThemeBlueColor,
+                title:'Login',
+                onPress: () {},
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/registration_screen');
+                    },
+                    child: const Text('New User? Register Here',  style: kDarkListingInputDecorationStyle),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/consts.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+const kThemeBlueColor = Color(0XFF77D4FC);
+
+const kThemeBlackColor = Color(0XFF000000);
+
+const kLightTitleColor = Color(0XFF697489);
+
+const kDarkTitleColor = Color(0XFF3A4355);
+
+const kBackgroundColor = Color(0XFFE3E6EF);
+
+const kBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+);
+
+var kEnabledBorder = const OutlineInputBorder(
+  borderSide: BorderSide(color: kLightTitleColor, width: 1.0),
+  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+);
+
+var kFocusedBorder = const OutlineInputBorder(
+  borderSide: BorderSide(color: kThemeBlackColor, width: 2.0),
+  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+);
+
+const kLightSemiBoldTextStyle = TextStyle(
+    color: kLightTitleColor,
+    fontSize: 16,
+    fontWeight:FontWeight.w500
+);
+
+const kDarkListingInputDecorationStyle = TextStyle(
+    color: kDarkTitleColor,
+    fontSize: 17,
+    fontWeight:FontWeight.w500
+);
+
+const kBlackListingInputDecorationStyle = TextStyle(
+    color: kThemeBlackColor,
+    fontSize: 17,
+    fontWeight:FontWeight.w500
+);
+
+InputDecoration passwordInputDecoration(String labelText, bool passwordVisible, void Function() toggle){
+  return InputDecoration(
+    labelText: labelText,
+    labelStyle: kDarkListingInputDecorationStyle,
+    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: kBorder,
+    enabledBorder: kEnabledBorder,
+    focusedBorder: kFocusedBorder,
+    suffixIcon: IconButton(
+      icon: Icon(
+        passwordVisible ? Icons.visibility : Icons.visibility_off,
+        color: kThemeBlackColor,
+      ),
+      onPressed: toggle,
+    ),
+  );
+}
+
+InputDecoration textInputDecoration(String labelText) {
+  return InputDecoration(
+    // hintText: hintText,
+    hintStyle: const TextStyle(color: kDarkTitleColor),
+    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+    border: kBorder,
+    enabledBorder: kEnabledBorder,
+    focusedBorder: kFocusedBorder,
+    labelText: labelText,
+    labelStyle: kDarkListingInputDecorationStyle,
+  );
+}
+
+class HeroLogo extends StatelessWidget {
+  HeroLogo({required this.height,required this.image, required this.tag});
+
+  final double height;
+  final String image, tag;
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag:tag,
+      child: Container(
+        height: height,
+        child: Image.asset(image),
+      ),
+    );
+  }
+}
