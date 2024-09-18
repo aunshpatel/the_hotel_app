b324ed2c44117fd25d9614396c3a8b2acde81914
aunshpatel <patelaunsh28@gmail.com>
2024-09-18 03:59:00 -0400

    Create 2 page base layout

---
+++ lib/guest_dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GuestDashboard extends StatefulWidget {
+  const GuestDashboard({super.key});
+
+  @override
+  State<GuestDashboard> createState() => _GuestDashboardState();
+}
+
+class _GuestDashboardState extends State<GuestDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
