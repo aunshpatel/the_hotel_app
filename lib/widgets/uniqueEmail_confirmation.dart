27e2af90bc37b427a896d0764a0534c98816bebf
aunshpatel <patelaunsh28@gmail.com>
2024-09-19 12:40:00 -0400

    Code cleanup

--- lib/widgets/uniqueEmail_confirmation.dart
+++ lib/widgets/uniqueEmail_confirmation.dart
@@ -2,10 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<bool> isEmailAlreadyRegistered(String email) async {
   try {
-    // Query Firestore for a document where the email field matches
     QuerySnapshot querySnapshot = await FirebaseFirestore.instance.collection('registered_user').where('email', isEqualTo: email).limit(1).get();
-
-    // If the query snapshot is not empty, the email already exists
     return querySnapshot.docs.isNotEmpty;
   } catch (e) {
     print('Error checking email: $e');
