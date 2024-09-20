174512b6a28432f15ce35050bc89fb169867db10
aunshpatel <patelaunsh28@gmail.com>
2024-09-19 20:41:22 -0400

    Code cleanup

--- lib/room_listings.dart
+++ lib/room_listings.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:the_hotel_app/widgets/consts.dart';
 import 'package:the_hotel_app/widgets/side_drawer.dart';
-import 'package:the_hotel_app/widgets/uniqueEmail_confirmation.dart';
 
 class RoomListings extends StatefulWidget {
   const RoomListings({super.key});
@@ -10,208 +9,6 @@ class RoomListings extends StatefulWidget {
   @override
   State<RoomListings> createState() => _RoomListingsState();
 }
-
-// class _RoomListingsState extends State<RoomListings> {
-//   final ScrollController _scrollController = ScrollController();
-//   CollectionReference roomData = FirebaseFirestore.instance.collection('room_data');
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: isDarkModeEnabled == false ? Colors.white : Colors.grey,
-//       drawer: const SideDrawer(),
-//       appBar: AppBar(
-//         centerTitle: true,
-//         title: Text('All Rooms', style:TextStyle(color: isDarkModeEnabled == false ? kThemeBlackColor : kThemeBlueColor),),
-//         backgroundColor: isDarkModeEnabled == false ? kThemeBlueColor : kThemeBlackColor,
-//         iconTheme: IconThemeData(color: isDarkModeEnabled == false ? kThemeBlackColor : kThemeBlueColor),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(20),
-//         child: Column(
-//           children: [
-//             StreamBuilder<QuerySnapshot>(
-//               stream: roomData.snapshots(),
-//               builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-//                 if (snapshot.connectionState == ConnectionState.waiting) {
-//                   return const Center(child: CircularProgressIndicator());
-//                 }
-//
-//                 if (snapshot.hasError) {
-//                   return Center(child: Text('Error: ${snapshot.error}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18));
-//                 }
-//
-//                 if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-//                   return Center(
-//                     child: Column(
-//                       children: [
-//                         Text('No Rooms Found!', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                         MaterialButton(
-//                           onPressed: () {
-//                             Navigator.pushNamed(context, '/add_new_room');
-//                           },
-//                           child: Text('Click Here to Add New Rooms!', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                         )
-//                       ],
-//                     )
-//                   );
-//                 }
-//
-//                 final items = snapshot.data!.docs;
-//                 return SizedBox(
-//                   height: MediaQuery.of(context).size.height - 145,
-//                   child: Scrollbar(
-//                     controller: _scrollController,
-//                     thumbVisibility: true,
-//                     child: ListView.builder(
-//                       controller: _scrollController,
-//                       scrollDirection: Axis.vertical,
-//                       shrinkWrap: true,
-//                       padding: const EdgeInsets.all(4),
-//                       itemCount: items.length,
-//                       itemBuilder: (BuildContext context, int index) {
-//                         String currencyType = items[index]['currencyType'].split(' - ')[0];
-//                         List<dynamic> amenities = items[index]['availableAmenities'] ?? [];
-//                         String amenitiesText = amenities.join(', ');
-//                         return Column(
-//                           children: [
-//                             Row(
-//                               mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                               children: [
-//                                 Card(
-//                                   child: SizedBox(
-//                                     width: 300,
-//                                     child: Padding(
-//                                       padding: const EdgeInsets.all(14),
-//                                       child: Column(
-//                                         crossAxisAlignment: CrossAxisAlignment.stretch,
-//                                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                                         children: [
-//                                           Image.network(items[index]['images'][0], height: 140,),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Room Number:', style: isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text(items[index]['roomNumber'].toString(), style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Description:', style: isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['description']}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18, style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Rent (per night):', style: isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('$currencyType ${items[index]['rent']}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18, style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Availability:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['availability']}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18, style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Bed & Bed Size:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['bedQuantity']} Beds ${items[index]['bedType']} Size', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Max Occupancy:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['maximumPeople'].toString()} People', style:isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Room Type:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text(items[index]['roomType'].toString(), style:isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('View Type:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text(items[index]['viewType'].toString(), style:isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Amenities:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text(amenitiesText, style:isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Smoking Policy:', style:isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['smokingPolicy']}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18, style:isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                           Column(
-//                                             mainAxisAlignment: MainAxisAlignment.start,
-//                                             crossAxisAlignment: CrossAxisAlignment.start,
-//                                             children: [
-//                                               Text('Cancellation Policy:', style: isDarkModeEnabled == false ? kBoldDarkTextSize18 : kBoldLightTextSize18),
-//                                               Text('${items[index]['cancellationPolicy']}', style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18, style: isDarkModeEnabled == false ? kDarkTextSize18 : kLightTextSize18),
-//                                             ],
-//                                           ),
-//                                           const SizedBox(height: 6,),
-//                                         ],
-//                                       ),
-//                                     ),
-//                                   ),
-//                                 ),
-//                               ],
-//                             ),
-//                             const SizedBox(
-//                               height: 20,
-//                             )
-//                           ],
-//                         );
-//                       }
-//                     )
-//                   ),
-//                 );
-//               },
-//             )
-//           ],
-//         )
-//       ),
-//     );
-//   }
-// }
 
 class _RoomListingsState extends State<RoomListings> {
   final ScrollController _scrollController = ScrollController();
@@ -224,14 +21,12 @@ class _RoomListingsState extends State<RoomListings> {
   @override
   void initState() {
     super.initState();
-    // Set default values for rent filters
     minRentController.text = minRent.toString();
     maxRentController.text = maxRent.toString();
   }
 
   @override
   void dispose() {
-    // Dispose controllers when not needed
     minRentController.dispose();
     maxRentController.dispose();
     super.dispose();
